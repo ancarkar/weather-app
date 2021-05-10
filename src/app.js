779606e3b2aec0877cec728422d16ba4b1965099
ancarkar <anabell.gemmersdoerfer@gmail.com>
2021-05-10 09:17:52 +0200

    Forecast Starting Day adjustments

--- src/app.js
+++ src/app.js
@@ -96,7 +96,7 @@ function showForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row row-cols-3">`;
   sixDayForecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index > 0 && index < 7) {
       let classes = "";
       if (index % 3 !== 2) {
         classes += "border-end ";
